e62dcf7b298876d908bfbb572e8f6303bf83dd05
orpheusohms <ominiabohsdavid@gmail.com>
2024-06-01 14:50:38 +0100

    add county cast

---
+++ src/Casts/CountryCast.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Orpheus\LaravelCountries\Casts;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Orpheus\LaravelCountries\Facades\Countries;
+
+class CountryCast implements CastsAttributes
+{
+    public $isoCodeVersion;
+
+    /**
+     * Create a new cast instance.
+     *
+     * @param  string  $isoCodeVersion The ISO code version to use (alpha2, alpha3, numeric)
+     */
+    public function __construct(
+        $isoCodeVersion = 'alpha2' // alpha2, alpha3, numeric
+    ) {
+        $this->isoCodeVersion = $isoCodeVersion;
+    }
+
+    /**
+     * Cast the given value.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function get(Model $model, string $key, $value, array $attributes)
+    {
+        if (is_null($value)) {
+            return null;
+        }
+
+        if ($this->isoCodeVersion === 'alpha3') {
+            return Countries::getByAlpha3Code($value);
+        }
+
+        if ($this->isoCodeVersion === 'numeric') {
+            return Countries::getByNumericCode($value);
+        }
+
+        return Countries::getByAlpha2Code($value);
+    }
+
+    public function set($model, string $key, $value, array $attributes)
+    {
+        if ($value instanceof \Orpheus\LaravelCountries\Country) {
+            if ($this->isoCodeVersion === 'alpha3') {
+                return $value->getAlpha3Code();
+            }
+
+            if ($this->isoCodeVersion === 'numeric') {
+                return $value->getNumericCode();
+            }
+
+            return $value->getAlpha2Code();
+        }
+
+        return $value;
+    }
+}
